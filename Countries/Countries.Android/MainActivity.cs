36d478569f1fa7a063ff1dcf546ede3e181b432d
Antonio Tamez Salinas <antonio.tamez@hotmail.com>
2019-02-18 12:05:13 -0600

    showing flags

--- Countries/Countries.Android/MainActivity.cs
+++ Countries/Countries.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using FFImageLoading.Forms.Droid;
 
 namespace Countries.Droid
 {
@@ -18,8 +19,8 @@ namespace Countries.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(true);
             LoadApplication(new App());
         }
     }
