ffee67cc6834fe0d25f36494fac9ab0d4ffd5713
Antonio Tamez Salinas <antonio.tamez@hotmail.com>
2019-02-18 11:42:57 -0600

    HorizontalList work

---
+++ Countries/Countries/Views/Template/FlagTemplate.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Countries.Views.Template
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FlagTemplate : ContentView
+    {
+        public FlagTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
